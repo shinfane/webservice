f26d709fa5f027300cce896aa74db1eadfe9f785
Shinfane <shinfane@gmx.de>
2013-02-16 21:08:16 +0100

    changed call for raspberry pi

--- remote.php
+++ remote.php
@@ -7,58 +7,58 @@
 $(document).ready(function(){
 	
   $("#FORWARD").mousedown(function(){
-   //$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=1"); //for items with the ID Forward, we send '1' on mousedown
-	$.get("/~micha/webservice/m.php?role=remote&order=1");
+   //$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=1"); //for items with the ID Forward, we send '1' on mousedown
+	$.get("/ws/m.php?role=remote&order=1");
   });
 $("#BACKWARD").mousedown(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=2") //For items with the ID Backward, we send '2' on mousedown
-	$.get("/~micha/webservice/m.php?role=remote&order=2");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=2") //For items with the ID Backward, we send '2' on mousedown
+	$.get("/ws/m.php?role=remote&order=2");
   });
 
 $("#LEFT").mousedown(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=3") //For items with the ID Left, we send '3' on mousedown
-	$.get("/~micha/webservice/m.php?role=remote&order=3");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=3") //For items with the ID Left, we send '3' on mousedown
+	$.get("/ws/m.php?role=remote&order=3");
   });
 
 $("#RIGHT").mousedown(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=4") //For items with the ID Right, we send '4' on mousedown
-    $.get("/~micha/webservice/m.php?role=remote&order=4");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=4") //For items with the ID Right, we send '4' on mousedown
+    $.get("/ws/m.php?role=remote&order=4");
   });
 
 $("#STOP").mousedown(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=0");//For items with the ID Stop, we send '0' on mousedown
-	$.get("/~micha/webservice/m.php?role=remote&order=0");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=0");//For items with the ID Stop, we send '0' on mousedown
+	$.get("/ws/m.php?role=remote&order=0");
   });
 
 $("#RELEASE").mousedown(function(){
-  	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=5") //For items with the ID Release, we send '5' on mousedown
-	$.get("/~micha/webservice/m.php?role=remote&order=5");
+  	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=5") //For items with the ID Release, we send '5' on mousedown
+	$.get("/ws/m.php?role=remote&order=5");
     });
 
 $("#STATUS").mousedown(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
-	$.get("/~micha/webservice/m.php?role=remote&order=6");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
+	$.get("/ws/m.php?role=remote&order=6");
 	 });
 $("#LSCAN").mousedown(function(){
-	 	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
-	$.get("/~micha/webservice/m.php?role=remote&order=7");
+	 	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
+	$.get("/ws/m.php?role=remote&order=7");
 	});
 		 
 $("#RSCAN").mousedown(function(){
-		 	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
-	$.get("/~micha/webservice/m.php?role=remote&order=8");
+		 	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
+	$.get("/ws/m.php?role=remote&order=8");
 	});
 
 $("#UPLOAD").mousedown(function(){
-			 	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
+			 	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=6") 
 	$.get("http://192.168.1.138/",function(data) { alert("Data Loaded: " + data)});
 	});
 
 
 
 $(".remoteControl").mouseup(function(){
-	//$.get("/~micha/webservice/m.php?role=c_w&cn=nxtmove&key=move&value=99") //For all items with the class remoteControl, we send 0 on mouseup
-	$.get("/~micha/webservice/m.php?role=remote&order=99");
+	//$.get("/ws/m.php?role=c_w&cn=nxtmove&key=move&value=99") //For all items with the class remoteControl, we send 0 on mouseup
+	$.get("/ws/m.php?role=remote&order=99");
   });
 
 });
